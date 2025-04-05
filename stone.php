a6855c7c40c8906e661d7630cc20bb07fcc94b2b
seegostone <ssewante348@gmail.com>
2025-04-05 16:01:15 +0300

    php code

---
+++ stone.php
@@ -0,0 +1,21 @@
+<?php
+$stonez="ssewante living stone";
+
+    $ssewante = explode("-",$stonez);
+
+foreach( $ssewante as $tk){
+
+    echo" {$tk}<br>";
+} 
+
+$stonez=array("ssewante", "living", "stone");
+
+    $ssewante = implode("-",$stonez);
+
+
+
+    echo $ssewante;
+ 
+
+
+?>
